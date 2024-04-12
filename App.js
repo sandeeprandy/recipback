82b5674a658b2879fa41b5221b1394ccbdecf308
sandeepdasari <sandeeprandi123@gmail.com>
2024-04-12 11:29:16 +0530

    upadted bcrypet

--- App.js
+++ App.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const cors = require('cors');
 const sqlite3 = require('sqlite3').verbose();
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 const app = express();
